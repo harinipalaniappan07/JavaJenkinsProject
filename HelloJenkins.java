0da4fa57a21384f6f953c3151e4f204c20f99130
Harini <harinipalaniappan@gmail.com>
2026-08-19 13:47:44 +0530

    Added Java Jenkins program

---
+++ HelloJenkins.java
@@ -0,0 +1,7 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+        System.out.println("Java program successfully built from GitHub.");
+        System.out.println("Jenkins Trigger Test Successful.");
+    }
+}
