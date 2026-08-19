8670aa428adabe9b950cf2b481a555541635b72e
Harini <harinipalaniappan@gmail.com>
2026-08-19 14:06:45 +0530

    Test Poll SCM trigger

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -2,6 +2,6 @@ public class HelloJenkins {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
         System.out.println("Java program successfully built from GitHub.");
-        System.out.println("Jenkins Trigger Test Successful.");
+        System.out.println("Poll SCM Trigger Test Successful.");
     }
 }
